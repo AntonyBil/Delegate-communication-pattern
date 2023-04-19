e4b48920b77592e6a5120f888bcad62fa78d5969
AntonyBil <beletskiy.ant@gmail.com>
2023-04-19 18:31:01 +0300

    Initial commit

---
+++ 8_Delegate/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  8_Delegate
+//
+//  Created by apple on 19.04.2023.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+    }
+
+
+}
+
